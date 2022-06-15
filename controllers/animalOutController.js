da1095d922ac45d1dd0dc03b5dcf4523a5844a80
preva878 <prevotadrian@gmail.com>
2022-06-15 14:21:16 +0200

    comit

---
+++ controllers/animalOutController.js
@@ -0,0 +1,14 @@
+const db = require('../Models')
+
+//image
+const multer = require('multer')
+const path = require ('path')
+
+//creation
+const AnimalOut = db.AnimalOut
+
+//1. addanimalOut
+
+const addAnimalOut = async (req,res) => {
+    
+}
